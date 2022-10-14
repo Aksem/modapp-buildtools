78ab6c3a341620753652d34db56b2dcce2af96fb
Vladyslav Hnatiuk <aders1234@gmail.com>
2022-10-14 15:08:37 +0200

    v0.1.5: add support of older patchelf

--- modapp_buildtools/rpath_utils.py
+++ modapp_buildtools/rpath_utils.py
@@ -32,7 +32,6 @@ def add_relative_rpath_if_needed(file_path: Path, to_path: Path) -> None:
     # from file to directory
     relative_path = Path(os.path.relpath(to_path, file_path.parent))
     rpath = f"$ORIGIN/{str(relative_path)}"
-    print("->", file_path, rpath)
     add_rpath_if_needed(file_path, rpath)
 
 
@@ -50,13 +49,21 @@ def add_rpath(file_path: Path, rpath: str) -> None:
         return
 
     commands_by_system = {
-        "Linux": 'patchelf --add-rpath "{rpath}" "{file_to_fix}"',
+        # Older systems that are often used for build include patchelf 0.9 which doesn't support
+        # --add-rpath. It was introduced in patchelf 0.14 that cannot be built for old systems,
+        # because it requires C++17. Use set + print instead of add.
+        "Linux": (
+            'patchelf --set-rpath "$(patchelf --print-rpath \'{file_to_fix}\'):{rpath}"'
+            ' "{file_to_fix}"'
+        ),
         "Darwin": 'install_name_tool -add_rpath "{rpath}" "{file_to_fix}"',
     }
-    
+
     add_rpath_command = commands_by_system[current_system].format(
         rpath=rpath, file_to_fix=file_path
     )
     exit_code, output = command_runner(add_rpath_command)
     if exit_code != 0:
         logger.error(f"Failed to add rpath '{rpath}' to '{file_path}': {output}")
+    else:
+        logger.trace(f"Added rpath '{rpath}' to '{file_path}'")
